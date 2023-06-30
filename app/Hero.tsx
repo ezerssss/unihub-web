6da584497a917d687770943eed80d5175f6cacf4
Ezra Magbanua <magbanuaezra@gmail.com>
2023-06-30 12:11:09 +0800

    [Setup][Ezra] tailwind prettier

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -5,18 +5,18 @@ import Image from 'next/image';
 function Hero() {
 	return (
 		<div className="flex flex-col items-center">
-			<h1 className="text-primary-400 text-2xl font-bold text-center sm:text-5xl mt-12 mx-12 sm:mx-24 sm:leading-normal">
+			<h1 className="mx-12 mt-12 text-center text-2xl font-bold text-primary-400 sm:mx-24 sm:text-5xl sm:leading-normal">
 				Making in-campus buying and selling easy
 			</h1>
 			<PrimaryButton className="mt-4">GET EARLY ACCESS</PrimaryButton>
 			<Image
 				alt="Mockup 1"
-				className="mt-6 sm:w-56 sm:h-fit"
+				className="mt-6 sm:h-fit sm:w-56"
 				height={226}
 				src="/mockup1.png"
 				width={160}
 			/>
-			<p className="text-dark text-center text-xs mx-20 mt-6 mb-14 sm:text-base sm:mx-48">
+			<p className="mx-20 mb-14 mt-6 text-center text-xs text-dark sm:mx-48 sm:text-base">
 				Save <span className="text-primary-400">time</span> and{' '}
 				<span className="text-primary-400">money</span> by connecting with
 				fellow students in your campus.
--- app/components/PrimaryButton.tsx
+++ app/components/PrimaryButton.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
 interface PrimaryButtonProps {
-  className?: string;
-  children?: React.ReactNode;
+	className?: string;
+	children?: React.ReactNode;
 }
 
 function PrimaryButton(props: PrimaryButtonProps) {
-  const { className, children} = props
-  return (
-    <button className={`text-primary-400 w-40 sm:w-52 h-10 sm:h-12 bg-secondary-100 text-xs sm:text-base font-semibold rounded-md ${className}`}>
-      {children}
-    </button>
-  );
+	const { className, children } = props;
+	return (
+		<button
+			className={`h-10 w-40 rounded-md bg-secondary-100 text-xs font-semibold text-primary-400 sm:h-12 sm:w-52 sm:text-base ${className}`}
+		>
+			{children}
+		</button>
+	);
 }
 
 export default PrimaryButton;
